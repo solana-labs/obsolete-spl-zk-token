bb2b73c97ad665df8f0302f178d5d3bb142d9cf1
Sam Kim <skim13@cs.stanford.edu>
2021-09-22 10:22:44 -0400

    add deposit.rs

---
+++ crypto/src/instruction/deposit.rs
@@ -0,0 +1,36 @@
+
+#[cfg(not(target_arch = "bpf"))]
+use crate::encryption::elgamal::ElGamal;
+use crate::errors::ProofError;
+
+pub struct DepositData {
+    pub amount: u64, // using u64 for now; add PodU64 to crate::pod?
+    pub decimals: u8,
+}
+
+impl DepositData {
+    #[cfg(not(target_arch = "bpf"))]
+    pub fn new(amount: u64, decimals: u8) -> Self {
+        DepositData {
+            amount,
+            decimals,
+        }
+    }
+}
+
+pub fn process_deposit(data: DepositData) -> Result<(), ProofError> {
+    // ...
+
+    // for demonstration purposes
+    let (source_pk, _) = ElGamal::keygen();
+    let current_pending_balance = source_pk.encrypt(55_u64).into();
+
+    // 1. check that the instruction data verified correctly
+
+    // 2. add `data.amount` to `current_pending_balance` as syscall
+    let _final_pending_balance = crate::pod::add_to_pod_ciphertext(current_pending_balance, data.amount);
+
+    // 3. store `final_pending_balance` in the destination zk-token account
+
+    Ok(())
+}
