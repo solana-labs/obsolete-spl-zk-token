5bf80d2b4ae882337dccf36d53b57511736d8851
Sam Kim <skim13@cs.stanford.edu>
2021-09-28 17:48:43 -0400

    add a generic pod arithmetic function for syscall

--- crypto/src/pod.rs
+++ crypto/src/pod.rs
@@ -1,4 +1,6 @@
 //! Plain Old Data wrappers for types that need to be sent over the wire
+
+use curve25519_dalek::constants::RISTRETTO_BASEPOINT_COMPRESSED;
 use {
     crate::{
         encryption::elgamal::{ElGamalCT, ElGamalPK},
@@ -70,6 +72,15 @@ impl TryFrom<PodElGamalCT> for ElGamalCT {
 
     fn try_from(pod: PodElGamalCT) -> Result<Self, Self::Error> {
         Self::from_bytes(&pod.0).ok_or(ProofError::InconsistentCTData)
+    }
+}
+
+impl From<(PodPedersenComm, PodPedersenDecHandle)> for PodElGamalCT {
+    fn from((pod_comm, pod_decrypt_handle): (PodPedersenComm, PodPedersenDecHandle)) -> Self {
+        let mut buf = [0_u8; 64];
+        buf[..32].copy_from_slice(&pod_comm.0);
+        buf[32..64].copy_from_slice(&pod_decrypt_handle.0);
+        PodElGamalCT(buf)
     }
 }
 
@@ -255,86 +266,64 @@ impl TryFrom<PodRangeProof128> for RangeProof {
 
 pub const TWO_32: u64 = 4294967296;
 
-// this function is needed for `ApplyPendingBalance`
+// Just this function needs syscall support
+//
+// On input two scalars x0, x1 and two ciphertexts ct0, ct1, the function returns x0*ct0 + x1*ct1
 pub fn add_pod_ciphertexts(
+    scalar_0: Scalar,
     pod_ct_0: PodElGamalCT,
+    scalar_1: Scalar,
     pod_ct_1: PodElGamalCT,
 ) -> Result<PodElGamalCT, ProofError> {
     let ct_0: ElGamalCT = pod_ct_0.try_into()?;
     let ct_1: ElGamalCT = pod_ct_1.try_into()?;
 
-    let ct_sum = ct_0 + ct_1;
+    let ct_sum = ct_0 * scalar_0 + ct_1 * scalar_1;
     Ok(ct_sum.into())
 }
 
-// it seems like this function is not used anywhere
-pub fn sub_pod_ciphertexts(
-    pod_ct_0: PodElGamalCT,
-    pod_ct_1: PodElGamalCT,
-) -> Result<PodElGamalCT, ProofError> {
-    let ct_0: ElGamalCT = pod_ct_0.try_into()?;
-    let ct_1: ElGamalCT = pod_ct_1.try_into()?;
+// All methods here rely on `general_add_pod_ciphertexts`
+pub struct PodElGamalArithmetic;
+impl PodElGamalArithmetic {
+    pub fn add(pod_ct_0: PodElGamalCT, pod_ct_1: PodElGamalCT) -> Result<PodElGamalCT, ProofError> {
+        add_pod_ciphertexts(Scalar::one(), pod_ct_0, Scalar::one(), pod_ct_1)
+    }
 
-    let ct_sub = ct_0 - ct_1;
-    Ok(ct_sub.into())
-}
+    pub fn subtract(
+        pod_ct_0: PodElGamalCT,
+        pod_ct_1: PodElGamalCT,
+    ) -> Result<PodElGamalCT, ProofError> {
+        add_pod_ciphertexts(Scalar::one(), pod_ct_0, -Scalar::one(), pod_ct_1)
+    }
 
-// this function is needed for transfer
-pub fn add_pod_ciphertexts_for_transfer(
-    pod_comm_lo: PodPedersenComm,
-    pod_dec_handle_lo: PodPedersenDecHandle,
-    pod_comm_hi: PodPedersenComm,
-    pod_dec_handle_hi: PodPedersenDecHandle,
-    pod_ct_dest: PodElGamalCT,
-) -> Result<PodElGamalCT, ProofError> {
-    let comm_lo: PedersenComm = pod_comm_lo.try_into()?;
-    let dec_handle_lo: PedersenDecHandle = pod_dec_handle_lo.try_into()?;
-    let ct_lo = dec_handle_lo.to_elgamal_ctxt(comm_lo); // convert into ElGamalCT
+    pub fn combine_lo_hi(
+        pod_ct_lo: PodElGamalCT,
+        pod_ct_hi: PodElGamalCT,
+    ) -> Result<PodElGamalCT, ProofError> {
+        add_pod_ciphertexts(Scalar::one(), pod_ct_lo, Scalar::from(TWO_32), pod_ct_hi)
+    }
 
-    let comm_hi: PedersenComm = pod_comm_hi.try_into()?;
-    let dec_handle_hi: PedersenDecHandle = pod_dec_handle_hi.try_into()?;
-    let ct_hi = dec_handle_hi.to_elgamal_ctxt(comm_hi); // convert into ElGamalCT
+    pub fn add_to(pod_ct: PodElGamalCT, amount: u64) -> Result<PodElGamalCT, ProofError> {
+        let mut buf = [0_u8; 64];
+        buf.copy_from_slice(RISTRETTO_BASEPOINT_COMPRESSED.as_bytes());
+        add_pod_ciphertexts(
+            Scalar::one(),
+            pod_ct,
+            Scalar::from(amount),
+            PodElGamalCT(buf),
+        )
+    }
 
-    let ct_dest: ElGamalCT = pod_ct_dest.try_into()?;
-
-    let ct_sum = ct_dest + (ct_lo + ct_hi * Scalar::from(TWO_32));
-    Ok(ct_sum.into())
-}
-
-// this function is needed for transfer
-pub fn sub_pod_ciphertexts_for_transfer(
-    pod_comm_lo: PodPedersenComm,
-    pod_dec_handle_lo: PodPedersenDecHandle,
-    pod_comm_hi: PodPedersenComm,
-    pod_dec_handle_hi: PodPedersenDecHandle,
-    pod_ct_dest: PodElGamalCT,
-) -> Result<PodElGamalCT, ProofError> {
-    let comm_lo: PedersenComm = pod_comm_lo.try_into()?;
-    let dec_handle_lo: PedersenDecHandle = pod_dec_handle_lo.try_into()?;
-    let ct_lo = dec_handle_lo.to_elgamal_ctxt(comm_lo); // convert into ElGamalCT
-
-    let comm_hi: PedersenComm = pod_comm_hi.try_into()?;
-    let dec_handle_hi: PedersenDecHandle = pod_dec_handle_hi.try_into()?;
-    let ct_hi = dec_handle_hi.to_elgamal_ctxt(comm_hi); // convert into ElGamalCT
-
-    let ct_dest: ElGamalCT = pod_ct_dest.try_into()?;
-
-    let ct_sub = ct_dest - (ct_lo + ct_hi * Scalar::from(TWO_32));
-    Ok(ct_sub.into())
-}
-
-// this function is needed for `Deposit`
-pub fn add_to_pod_ciphertext(ct: PodElGamalCT, amount: u64) -> Result<PodElGamalCT, ProofError> {
-    let ct: ElGamalCT = ct.try_into()?;
-    let ct_sum = ct.add_to_msg(amount);
-    Ok(ct_sum.into())
-}
-
-// this function is needed for `Withdraw`
-pub fn sub_to_pod_ciphertext(ct: PodElGamalCT, amount: u64) -> Result<PodElGamalCT, ProofError> {
-    let ct: ElGamalCT = ct.try_into()?;
-    let ct_sub = ct.sub_to_msg(amount);
-    Ok(ct_sub.into())
+    pub fn subtract_to(pod_ct: PodElGamalCT, amount: u64) -> Result<PodElGamalCT, ProofError> {
+        let mut buf = [0; 64];
+        buf.copy_from_slice(RISTRETTO_BASEPOINT_COMPRESSED.as_bytes());
+        add_pod_ciphertexts(
+            Scalar::one(),
+            pod_ct,
+            -Scalar::from(amount),
+            PodElGamalCT(buf),
+        )
+    }
 }
 
 #[cfg(test)]
@@ -370,7 +359,7 @@ mod tests {
         let transfer_amount_ct = pk.encrypt_with(55_u64, &open);
         let transfer_amount_pod: PodElGamalCT = transfer_amount_ct.into();
 
-        let sum = crate::pod::add_pod_ciphertexts(spendable_balance, transfer_amount_pod).unwrap();
+        let sum = PodElGamalArithmetic::add(spendable_balance, transfer_amount_pod).unwrap();
 
         let expected: PodElGamalCT = pk.encrypt_with(55_u64, &open).into();
         assert_eq!(expected, sum);
@@ -482,36 +471,42 @@ mod tests {
         let source_spendable_ct: PodElGamalCT = source_pk.encrypt_with(77_u64, &source_open).into();
         let dest_pending_ct: PodElGamalCT = dest_pk.encrypt_with(77_u64, &dest_open).into();
 
-        // program arithmetic
+        // program arithmetic for the source account
 
-        // subtracting from source
-        let final_source_spendable_ct = sub_pod_ciphertexts_for_transfer(
-            comm_lo,
-            handle_source_lo,
-            comm_hi,
-            handle_source_hi,
-            source_spendable_ct,
-        )
-        .unwrap();
+        // 1. Combine commitments and handles
+        let source_lo_ct: PodElGamalCT = (comm_lo, handle_source_lo).into();
+        let source_hi_ct: PodElGamalCT = (comm_hi, handle_source_hi).into();
 
+        // 2. Combine lo and hi ciphertexts
+        let source_combined_ct =
+            PodElGamalArithmetic::combine_lo_hi(source_lo_ct, source_hi_ct).unwrap();
+
+        // 3. Subtract from available balance
+        let final_source_spendable =
+            PodElGamalArithmetic::subtract(source_spendable_ct, source_combined_ct).unwrap();
+
+        // test
         let final_source_open =
             source_open - (open_lo.clone() + open_hi.clone() * Scalar::from(TWO_32));
-        let expected_source_ct: PodElGamalCT =
+        let expected_source: PodElGamalCT =
             source_pk.encrypt_with(22_u64, &final_source_open).into();
-        assert_eq!(expected_source_ct, final_source_spendable_ct);
+        assert_eq!(expected_source, final_source_spendable);
 
-        // adding to destination
-        let final_dest_pending_ct = add_pod_ciphertexts_for_transfer(
-            comm_lo,
-            handle_dest_lo,
-            comm_hi,
-            handle_dest_hi,
-            dest_pending_ct,
-        )
-        .unwrap();
+        // same for the destination account
+
+        // 1. Combine commitments and handles
+        let dest_lo_ct: PodElGamalCT = (comm_lo, handle_dest_lo).into();
+        let dest_hi_ct: PodElGamalCT = (comm_hi, handle_dest_hi).into();
+
+        // 2. Combine lo and hi ciphertexts
+        let dest_combined_ct = PodElGamalArithmetic::combine_lo_hi(dest_lo_ct, dest_hi_ct).unwrap();
+
+        // 3. Add to pending balance
+        let final_dest_pending =
+            PodElGamalArithmetic::add(dest_pending_ct, dest_combined_ct).unwrap();
 
         let final_dest_open = dest_open + (open_lo + open_hi * Scalar::from(TWO_32));
         let expected_dest_ct: PodElGamalCT = dest_pk.encrypt_with(132_u64, &final_dest_open).into();
-        assert_eq!(expected_dest_ct, final_dest_pending_ct);
+        assert_eq!(expected_dest_ct, final_dest_pending);
     }
 }
