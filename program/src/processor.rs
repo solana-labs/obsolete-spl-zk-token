ef37457237969ca83163a04bbc420c3573a8ffb7
Michael Vines <mvines@gmail.com>
2021-10-25 22:28:24 -0700

    Rename `incoming_transfer_count` to `pending_balance_instructions`

--- program/src/processor.rs
+++ program/src/processor.rs
@@ -560,8 +560,8 @@ fn process_deposit(accounts: &[AccountInfo], amount: u64, decimals: u8) -> Progr
         ops::add_to(&confidential_account.pending_balance, amount)
             .ok_or(ProgramError::InvalidInstructionData)?;
 
-    confidential_account.incoming_transfer_count =
-        (u64::from(confidential_account.incoming_transfer_count) + 1).into();
+    confidential_account.pending_balance_instructions =
+        (u64::from(confidential_account.pending_balance_instructions) + 1).into();
 
     Ok(())
 }
@@ -744,8 +744,8 @@ fn process_transfer(accounts: &[AccountInfo], aes_ciphertext: pod::AesCiphertext
         .ok_or(ProgramError::InvalidInstructionData)?;
     }
 
-    receiver_confidential_account.incoming_transfer_count =
-        (u64::from(receiver_confidential_account.incoming_transfer_count) + 1).into();
+    receiver_confidential_account.pending_balance_instructions =
+        (u64::from(receiver_confidential_account.pending_balance_instructions) + 1).into();
 
     Ok(())
 }
@@ -753,7 +753,7 @@ fn process_transfer(accounts: &[AccountInfo], aes_ciphertext: pod::AesCiphertext
 /// Processes an [ApplyPendingBalance] instruction.
 fn process_apply_pending_balance(
     accounts: &[AccountInfo],
-    expected_incoming_transfer_count: u64,
+    expected_pending_balance_instructions: PodU64,
     aes_ciphertext: pod::AesCiphertext,
 ) -> ProgramResult {
     let account_info_iter = &mut accounts.iter();
@@ -769,21 +769,18 @@ fn process_apply_pending_balance(
         account_info_iter.as_slice(),
     )?;
 
-    confidential_account
-        .applied_incoming_transfer_count
-        .expected_incoming_transfer_count = expected_incoming_transfer_count.into();
-
-    confidential_account
-        .applied_incoming_transfer_count
-        .actual_incoming_transfer_count = confidential_account.incoming_transfer_count;
-
     confidential_account.available_balance = ops::add(
         &confidential_account.available_balance,
         &confidential_account.pending_balance,
     )
     .ok_or(ProgramError::InvalidInstructionData)?;
-    confidential_account.pending_balance = pod::ElGamalCiphertext::zeroed();
+
+    confidential_account.actual_applied_pending_balance_instructions =
+        confidential_account.pending_balance_instructions;
+    confidential_account.expected_apply_pending_balance_instructions =
+        expected_pending_balance_instructions;
     confidential_account.decryptable_balance = aes_ciphertext;
+    confidential_account.pending_balance = pod::ElGamalCiphertext::zeroed();
 
     Ok(())
 }
@@ -869,7 +866,7 @@ pub fn process_instruction(
 
             process_apply_pending_balance(
                 accounts,
-                data.expected_incoming_transfer_count.into(),
+                data.expected_pending_balance_instructions,
                 data.aes_ciphertext,
             )
         }
--- program/src/state.rs
+++ program/src/state.rs
@@ -29,8 +29,8 @@ pub struct ConfidentialAccount {
     pub mint: Pubkey,
 
     /// The SPL Token account that corresponds to this confidential token account.
-    /// The owner and close authority of the SPL Token account convey their authority over the
-    /// confidential token account
+    /// The owner of the SPL Token account convey their authority over the confidential token
+    /// account
     pub token_account: Pubkey,
 
     /// The public key associated with ElGamal encryption
@@ -48,87 +48,14 @@ pub struct ConfidentialAccount {
     /// Prohibit incoming transfers if `false`
     pub accept_incoming_transfers: PodBool,
 
-    /// Counts the number of incoming transfers
-    pub incoming_transfer_count: PodU64,
+    /// The total number of `Deposit` and `Transfer` instructions that have credited `pending_balance`
+    pub pending_balance_instructions: PodU64,
 
-    /// Record of `incoming_transfer_count` at the time of the most recent `ApplyPendingBalance`
-    pub applied_incoming_transfer_count: TransferCountRecord,
+    /// The expected `pending_balance_instructions` that was included in the last `ApplyPendingBalance`
+    /// instruction
+    pub expected_apply_pending_balance_instructions: PodU64,
+
+    /// The actual `pending_balance_instructions` at last time the `ApplyPendingBalance` was executed
+    pub actual_applied_pending_balance_instructions: PodU64,
 }
 impl PodAccountInfo<'_, '_> for ConfidentialAccount {}
-
-/// After submitting `ApplyPendingBalance`, the client should compare the expected and the actual
-/// transfer counts. If they are equal, then the `decryptable_balance` is consistent with
-/// `available_balance`. If they differ, then the client should update the `decryptable_balance`.
-#[repr(C)]
-#[derive(Clone, Copy, Pod, Zeroable)]
-pub struct TransferCountRecord {
-    /// The expected `incoming_transfer_count` that was included in the `ApplyPendingBalance`
-    /// instruction
-    pub expected_incoming_transfer_count: PodU64,
-
-    /// The actual `incoming_transfer_count` at the time the `ApplyPendingBalance` was executed
-    pub actual_incoming_transfer_count: PodU64,
-}
-
-#[cfg(test)]
-mod tests {
-    /*
-    use super::*;
-
-    #[test]
-    fn test_get_packed_len() {
-        assert_eq!(
-            Auditor::get_packed_len(),
-            solana_program::borsh::get_packed_len::<Auditor>()
-        );
-
-        assert_eq!(
-            ConfidentialAccount::get_packed_len(),
-            solana_program::borsh::get_packed_len::<ConfidentialAccount>()
-        );
-    }
-
-    #[test]
-    fn test_serialize_bytes() {
-        assert_eq!(FeatureProposal::Expired.try_to_vec().unwrap(), vec![3]);
-
-        assert_eq!(
-            FeatureProposal::Pending(AcceptanceCriteria {
-                tokens_required: 0xdeadbeefdeadbeef,
-                deadline: -1,
-            })
-            .try_to_vec()
-            .unwrap(),
-            vec![1, 239, 190, 173, 222, 239, 190, 173, 222, 255, 255, 255, 255, 255, 255, 255, 255],
-        );
-    }
-
-    #[test]
-    fn test_serialize_large_slice() {
-        let mut dst = vec![0xff; 4];
-        FeatureProposal::Expired.pack_into_slice(&mut dst);
-
-        // Extra bytes (0xff) ignored
-        assert_eq!(dst, vec![3, 0xff, 0xff, 0xff]);
-    }
-
-    #[test]
-    fn state_deserialize_invalid() {
-        assert_eq!(
-            FeatureProposal::unpack_from_slice(&[3]),
-            Ok(FeatureProposal::Expired),
-        );
-
-        // Extra bytes (0xff) ignored...
-        assert_eq!(
-            FeatureProposal::unpack_from_slice(&[3, 0xff, 0xff, 0xff]),
-            Ok(FeatureProposal::Expired),
-        );
-
-        assert_eq!(
-            FeatureProposal::unpack_from_slice(&[4]),
-            Err(ProgramError::InvalidAccountData),
-        );
-    }
-    */
-}
